fc81628cf4bbf02c616e8ab20bec99cfd02d4516
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2022-11-15 14:49:28 -0300

    Initial Commit

---
+++ projetoCOVID/projetoCOVIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetoCOVIDApp.swift
+//  projetoCOVID
+//
+//  Created by Roberto Edgar Geiss on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct projetoCOVIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
