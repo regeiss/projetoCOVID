c9e9cf68ad71184a673f8c248117bf94303369f8
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2023-01-13 09:48:35 -0300

    criação de arquivos

---
+++ projetoCOVID/Features/Model/MundialModel.swift
@@ -0,0 +1,8 @@
+//
+//  MundialModel.swift
+//  projetoCOVID
+//
+//  Created by Roberto Edgar Geiss on 13/01/23.
+//
+
+import Foundation
---
+++ projetoCOVID/Features/Model/PaisModel.swift
@@ -0,0 +1,8 @@
+//
+//  PaisModel.swift
+//  projetoCOVID
+//
+//  Created by Roberto Edgar Geiss on 13/01/23.
+//
+
+import Foundation
