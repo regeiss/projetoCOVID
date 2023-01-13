0578dfab59da4b24ced514f2aa18ee9f60b40c3e
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-01-13 12:14:04 -0300

    Update MundialModel.swift

--- projetoCOVID/Features/Model/MundialModel.swift
+++ projetoCOVID/Features/Model/MundialModel.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+
+struct MundialModel: Codable, Hashable 
+{
+    let populacao: Int
+    let casos: Int
+    let ativos: Int
+    let mortes: Int
+    let recuperados: Int
+    
+    // MARK: - Mundial Model Computed Properties
+    var statOfCasos: String 
+    {
+        if casos == 0 
+        {
+            return "N/A\n"
+        } 
+        else 
+        {
+            return "\(casos.numberFormat())\n" + "\(((Double(casos) * 100) / Double(population)).rounded(by: 2))% of the population"
+        }
+    }
+    
+    var statOfMortes: String 
+    {
+        if mortes == 0 
+        {
+            return "N/A\n"
+        } 
+        else 
+        {
+            return "\(mortes.numberFormat())\n" + "\(((Double(mortes) * 100) / Double(casos)).rounded(by: 2))% of total casos"
+        }
+    }
+    
+    var statOfAtivos: String 
+    {
+        if recuperados == 0 
+        {
+            return "N/A\n"
+        } 
+        else 
+        {
+            return "\(active.numberFormat())\n" + "\(((Double(active) * 100) / Double(population)).rounded(by: 2))% da população"
+        }
+    }
+    
+    var statOfRecuperados: String 
+    {
+        if recuperados == 0 
+        {
+            return "N/A\n"
+        } 
+        else 
+        {
+            return "\(recuperados.numberFormat())\n" + "\(((Double(recuperados) * 100) / Double(casos)).rounded(by: 2))% of total casos"
+        }
+    }
+}
