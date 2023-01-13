08d35c05f2c00286a1f9e397f58e3164d4b96a3d
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-01-13 12:20:29 -0300

    Update LinhaTempoModel.swift

--- projetoCOVID/Features/Model/LinhaTempoModel.swift
+++ projetoCOVID/Features/Model/LinhaTempoModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct TimelineModel: Codable, Hashable 
+{
+    let pais: String
+    let provincia: [String]
+    let linhatempo: TimelineInfoModel
+}
+
+struct LinhaTempoInfoModel: Codable, Hashable 
+{
+    let cases, deaths, recovered: [String: Int]
+}
+
+struct LinhaTempoData 
+{
+    var pais: String
+    var casesTimeline: Array<(key: Date, value: Int)>
+    var deathsTimeline: Array<(key: Date, value: Int)>
+}
+
+struct DiarioModel: Hashable 
+{
+    let uuid = UUID()
+
+    let dData: Date
+    let dCasos: Int
+    let dMortes: Int
+}
