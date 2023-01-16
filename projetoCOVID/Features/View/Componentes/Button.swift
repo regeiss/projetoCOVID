a7e450b332fabc6fe738831670b6be19304ab60f
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-01-16 17:20:38 -0300

    Update Button.swift

--- projetoCOVID/Features/View/Componentes/Button.swift
+++ projetoCOVID/Features/View/Componentes/Button.swift
@@ -5,4 +5,22 @@
 //  Created by Roberto Edgar Geiss on 13/01/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct OKXMarkButton: View 
+{
+    var body: some View 
+    {
+        ZStack 
+        {
+            Circle()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.secondary)
+                .opacity(0.2)
+            Image(systemName: "xmark")
+                .imageScale(.small)
+                .frame(width: 34, height: 34)
+                .foregroundColor(.primary)
+        }
+    }
+}
