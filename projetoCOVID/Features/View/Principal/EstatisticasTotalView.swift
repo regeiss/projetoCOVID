a1206a1ada07d7ec23e17ea7ef7db0d30b9dafcc
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-01-17 17:35:28 -0300

    Update EstatisticasTotalView.swift

--- projetoCOVID/Features/View/Principal/EstatisticasTotalView.swift
+++ projetoCOVID/Features/View/Principal/EstatisticasTotalView.swift
@@ -7,43 +7,46 @@
 
 import SwiftUI
 
-struct EstatisticasTotalView: View
+struct TotalStatsView: View 
 {
-//    @StateObject var viewModel = TotalStatsViewModel()
-//
-    var body: some View
+    @StateObject var viewModel = TotalStatsViewModel()
+    
+    var body: some View 
     {
-//        VStack
-//        {
-//            if viewModel.isLoading
-//            {
-//                ProgressView()
-//                    .onAppear(perform: { viewModel.getTotalStatsData()})
-//                    .progressViewStyle(CircularProgressViewStyle())
-//                    .scaleEffect(1.5)
-//            }
-//            else
-//            {
+        VStack 
+        {
+            if viewModel.isLoading 
+            {
+                ProgressView()
+                    .onAppear(perform: {
+                        viewModel.getTotalStatsData()
+                    })
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .scaleEffect(1.5)
+            } 
+            else 
+            {
                 Spacer()
-//                WorldStatsCard(worldData: viewModel.worldData ?? MockData.sampleWorldData)
-//                    .frame(maxWidth: 500)
-//                Spacer()
-//                ScrollView(.horizontal, showsIndicators: false)
-//                {
-//                    HStack(alignment: .top, spacing: 15)
-//                    {
-//                        ForEach(viewModel.countriesData, id: \.countryInfo._id) { country in
-//                            CountryStatsCard(countryData: country)
-//                                .onTapGesture {
-//                                    viewModel.selectedCountry = country
-//                                }
-//                        }
-//                    }
-//                    .padding(.leading, 15)
-//                }
-//                .sheet(isPresented: $viewModel.isShowingDetailView) { CountryDetailView(countryData: viewModel.selectedCountry ?? MockData.sampleCountryData, isShowingDetail: $viewModel.isShowingDetailView)}
-//            }
-//        }
-//        .blur(radius: viewModel.isShowingDetailView ? 5 : 0)
+                WorldStatsCard(worldData: viewModel.worldData ?? MockData.sampleWorldData)
+                    .frame(maxWidth: 500)
+                Spacer()
+                ScrollView(.horizontal, showsIndicators: false) 
+                {
+                    HStack(alignment: .top, spacing: 15) 
+                    {
+                        ForEach(viewModel.countriesData, id: \.countryInfo._id) { country in
+                            CountryStatsCard(countryData: country)
+                                .onTapGesture { viewModel.selectedCountry = country }
+                        }
+                    }
+                    .padding(.leading, 15)
+                }
+                .sheet(isPresented: $viewModel.isShowingDetailView) 
+                {
+                    CountryDetailView(countryData: viewModel.selectedCountry ?? MockData.sampleCountryData, isShowingDetail: $viewModel.isShowingDetailView)
+                }
+            }
+        }
+        .blur(radius: viewModel.isShowingDetailView ? 5 : 0)
     }
 }
