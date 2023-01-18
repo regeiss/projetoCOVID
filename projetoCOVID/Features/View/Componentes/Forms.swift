ddfa8e5e37c9340fc7936844bf36c2acb6cb230e
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2023-01-18 08:21:10 -0300

    inclusão arquivos

---
+++ projetoCOVID/Features/View/Componentes/Forms.swift
@@ -0,0 +1,15 @@
+//
+//  Forms.swift
+//  projetoCOVID
+//
+//  Created by Roberto Edgar Geiss on 18/01/23.
+//
+
+import SwiftUI
+
+struct Forms: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
---
+++ projetoCOVID/Features/View/Componentes/Outros.swift
@@ -0,0 +1,16 @@
+//
+//  Outros.swift
+//  projetoCOVID
+//
+//  Created by Roberto Edgar Geiss on 18/01/23.
+//
+
+import SwiftUI
+
+struct Outros: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
