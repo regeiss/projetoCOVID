97cf3a9a4019b924a4b4d1e3192cc2940db83927
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-01-18 09:54:04 -0300

    Update Forms.swift

--- projetoCOVID/Features/View/Componentes/Forms.swift
+++ projetoCOVID/Features/View/Componentes/Forms.swift
@@ -7,9 +7,50 @@
 
 import SwiftUI
 
-struct Forms: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CountryInfoForm: View 
+{
+    var countryData: CountryModel
+    var body: some View 
+    {
+        Form 
+        {
+            Section(header: Text(countryData.updatedLblText)) 
+            {
+                HStack 
+                {
+                    Image(systemName: "person.2")
+                        .frame(width: 20, height: 20, alignment: .center)
+                    Text("Population").foregroundColor(Color.gray)
+                    Spacer()
+                    Text(countryData.population.numberFormat())
+                }
+                HStack 
+                {
+                    Image(systemName: "eyedropper.halffull")
+                        .frame(width: 20, height: 20, alignment: .center)
+                    Text("Tests").foregroundColor(Color.gray)
+                    Spacer()
+                    Text(countryData.tests.numberFormat())
+                }
+                HStack 
+                {
+                    Image(systemName: "thermometer")
+                        .frame(width: 20, height: 20, alignment: .center)
+                    Text("Cases").foregroundColor(Color.gray)
+                    Spacer()
+                    Text(countryData.cases.numberFormat())
+                }
+                HStack 
+                {
+                    Image(systemName: "heart.slash")
+                        .frame(width: 20, height: 20, alignment: .center)
+                    Text("Deaths").foregroundColor(Color.gray)
+                    Spacer()
+                    Text(countryData.deaths.numberFormat())
+                }
+            }
+        }
+        .frame(height: 220, alignment: .top)
+        .disabled(true)
     }
 }
-
