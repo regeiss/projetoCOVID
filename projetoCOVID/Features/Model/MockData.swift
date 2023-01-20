b07e2cdebf1c8a94512ed45fab7c6a4306f5b420
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-01-20 13:12:20 -0300

    Update MockData.swift

--- projetoCOVID/Features/Model/MockData.swift
+++ projetoCOVID/Features/Model/MockData.swift
@@ -7,27 +7,10 @@
 
 import Foundation
 
-struct MockData {
-    static let sampleWorldData      = MundialModel(population: 7787962770,
-                                                 cases: 56342420,
-                                                 active: 15778135,
-                                                 deaths: 1350973,
-                                                 recovered: 39213312)
-    
-    static let sampleCountryInfo    = PaisInfo(_id: 840,
-                                                  flag: "https://disease.sh/assets/img/flags/us.png",
-                                                  iso2: "US",
-                                                  iso3: "USA",
-                                                  lat: 38,
-                                                  long: -97)
-    
-    static let sampleCountryData    = PaisModel(country: "USA",
-                                                   countryInfo: sampleCountryInfo,
-                                                   population: 331743344,
-                                                   updated: 1605727532041,
-                                                   tests: 172178586,
-                                                   cases: 11758671,
-                                                   active: 4389449,
-                                                   deaths: 255112,
-                                                   recovered: 7114110)
+struct MockData 
+{
+    static let sampleWorldData = MundialModel(population: 7787962770, cases: 56342420, active: 15778135, deaths: 1350973, recovered: 39213312)
+    static let sampleCountryInfo = PaisInfo(_id: 840, flag: "https://disease.sh/assets/img/flags/us.png", iso2: "US",  iso3: "USA", lat: 38, long: -97)
+    static let sampleCountryData = PaisModel(country: "USA", countryInfo: sampleCountryInfo, population: 331743344, updated: 1605727532041, tests: 172178586, cases: 11758671, 
+                                                   active: 4389449, deaths: 255112, recovered: 7114110)
 }
