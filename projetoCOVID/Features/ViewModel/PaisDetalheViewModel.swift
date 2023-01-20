f96ca33d4e3a32700b161bcf7be2e6fbad7c93e2
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-01-20 13:06:02 -0300

    Update PaisDetalheViewModel.swift

--- projetoCOVID/Features/ViewModel/PaisDetalheViewModel.swift
+++ projetoCOVID/Features/ViewModel/PaisDetalheViewModel.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-final class PaisDetalheViewModel: ObservableObject {
+final class PaisDetalheViewModel: ObservableObject 
+{
     @Published var dailyData: [DiarioModel] = []
     
-    func getDailyStatsData(for country: String) {
+    func getDailyStatsData(for country: String) 
+    {
         NetworkManager.shared.fetch(for: country, ifDaily: true) { (result: TimelineModel) in
             
-            let casesDict       = self.convertTimelineData(timeline: result.linhatempo.casos)
-            let deathsDict      = self.convertTimelineData(timeline: result.linhatempo.mortes)
-            let timelineData    = LinhaTempoData(pais: result.pais, casosLinhaTempo: casesDict, mortesLinhaTempo: deathsDict)
+            let casesDict = self.convertTimelineData(timeline: result.linhatempo.casos)
+            let deathsDict = self.convertTimelineData(timeline: result.linhatempo.mortes)
+            let timelineData = LinhaTempoData(pais: result.pais, casosLinhaTempo: casesDict, mortesLinhaTempo: deathsDict)
             
             DispatchQueue.main.async {
                 self.updateArray(with: timelineData)
@@ -23,31 +25,36 @@ final class PaisDetalheViewModel: ObservableObject {
         }
     }
     
-    func convertTimelineData(timeline: [String: Int]) -> Array<(key: Date, value: Int)> {
-        let dateFormatter           = DateFormatter()
-        dateFormatter.dateFormat    = "MM/dd/yy"
+    func convertTimelineData(timeline: [String: Int]) -> Array<(key: Date, value: Int)> 
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yy"
+        var dateDictionary = [Date: Int]()
         
-        var dateDictionary          = [Date: Int]()
-        
-        for (key, value) in timeline {
-            let date                = dateFormatter.date(from: key)
-            dateDictionary[date!]   = value
+        for (key, value) in timeline 
+        {
+            let date = dateFormatter.date(from: key)
+            dateDictionary[date!] = value
         }
         
         return dateDictionary.sorted { $0.0 < $1.0 }
     }
     
-    func updateArray(with timelineData: LinhaTempoData) {
-        for i in 1...timelineData.casosLinhaTempo.count - 1 {
+    func updateArray(with timelineData: LinhaTempoData) 
+    {
+        for i in 1...timelineData.casosLinhaTempo.count - 1 
+        {
             self.dailyData.append(.init(dDate: timelineData.casosLinhaTempo[i].key, dCases: timelineData.casosLinhaTempo[i].value - timelineData.casosLinhaTempo[i-1].value, dDeaths: timelineData.mortesLinhaTempo[i].value - timelineData.mortesLinhaTempo[i-1].value))
         }
         
-        var reversedNames   = [DiarioModel]()
+        var reversedNames = [DiarioModel]()
 
-        for name in dailyData {
+        for name in dailyData 
+        {
             reversedNames.insert(name, at: 0)
         }
         
-        self.dailyData     = reversedNames
+        self.dailyData = reversedNames
     }
 }
+
