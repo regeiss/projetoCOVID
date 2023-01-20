028458df810bb9f0973a1033eb7efc27e5956390
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-01-20 12:57:19 -0300

    Update EstatisticasTotalViewModel.swift

--- projetoCOVID/Features/ViewModel/EstatisticasTotalViewModel.swift
+++ projetoCOVID/Features/ViewModel/EstatisticasTotalViewModel.swift
@@ -7,34 +7,37 @@
 
 import SwiftUI
 
-final class EstatisticasTotalViewModel: ObservableObject {
+final class EstatisticasTotalViewModel: ObservableObject 
+{
     @Published var worldData: MundialModel?
-    @Published var countriesData: [PaisModel]    = []
-    @Published var isLoading                        = true
-    @Published var isShowingDetailView              = false
+    @Published var countriesData: [PaisModel] = []
+    @Published var isLoading = true
+    @Published var isShowingDetailView = false
     
-    var selectedCountry: PaisModel? {
-        didSet {
-            isShowingDetailView = true
-        }
+    var selectedCountry: PaisModel? 
+    {
+        didSet { isShowingDetailView = true }
     }
     
     private let countries = ["usa", "uk", "ita", "fr", "esp", "deu", "tr", "can", "au", "bra"]
     
-    func getTotalStatsData() {
+    func getTotalStatsData() 
+    {
         NetworkManager.shared.fetch(for: nil, ifDaily: false) { (result: MundialModel) in
             DispatchQueue.main.async {
                 self.worldData = result
             }
         }
         
-        for item in countries {
+        for item in countries 
+        {
             NetworkManager.shared.fetch(for: item, ifDaily: false) { (result: PaisModel) in
                 DispatchQueue.main.sync {
                     self.countriesData.append(result)
                     self.countriesData.sort { $1.cases < $0.cases }
                     
-                    if self.countriesData.count == self.countries.count {
+                    if self.countriesData.count == self.countries.count 
+                    {
                         self.isLoading = false
                     }
                 }
